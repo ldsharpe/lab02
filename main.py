4ae1dc8e1affb1c2ee985d6a4b37f080a67c03e1
Yuvraj Jain <yjain@unc.edu>
2025-11-20 09:12:56 -0500

    Latest changes

--- main.py
+++ main.py
@@ -1,50 +1,12 @@
 #!/usr/bin/env pybricks-micropython
 from robot import Robot
-<<<<<<< HEAD
 from pybricks.tools import wait, StopWatch
 
 # Lucas Sharpe - 730545934
 # Yuvraj Jain  - 730465868
-=======
-from pybricks.tools import wait
-import math
->>>>>>> efc0cf5e95e54e471201092bd498750626ea97d4
+
 
 def main():
-    bot = Robot()
-    bot.GYRO_W = 1.0
-
-    bot.wait_for_button_press()
-
-    bot.reset_odometry(x=0.5, y=0.0, theta=math.radians(90))
-
-<<<<<<< HEAD
-    bot.creep_forward(speed=200)
-    while not bot.touch_sensor_left.pressed() and not bot.touch_sensor_right.pressed():
-        bot.update_position()
-        wait(30)
-    bot.left_motor.stop(); bot.right_motor.stop()
-=======
-    bot.turn_towards_goal(2.5, 2.5)
-    bot.move_forward(0.5)
->>>>>>> efc0cf5e95e54e471201092bd498750626ea97d4
-
-    bot.turn(-70)
-    bot.move_forward(0.3)
-
-    bot.turn_towards_goal(2.5, 2.5)
-    bot.beep()
-
-    return_pos = [bot.get_x(), bot.get_y(), bot.get_theta()]
-
-    bot.wall_follow(start_pos, return_pos)
-
-    bot.turn(-90, 100)
-    bot.move_forward(abs(start_pos[0] - return_pos[0]))
-    bot.beep()
-
-
-def x():
     bot = Robot()
 
     bot.wait_for_button_press()
@@ -61,7 +23,7 @@ def x():
 
         while not bot.touch_sensor_left.pressed() and not bot.touch_sensor_right.pressed():
             bot.update_position()
-            bot.move(200, 200)
+            bot.move(350, 350)
             print(bot.get_x(), bot.get_y())
             wait(30)
             if bot.near_goal():
@@ -75,7 +37,7 @@ def x():
             break         
         
         bot.move_backward(0.18)
-        bot.turn(-90, 100)
+        bot.turn(-90, 200)
         
         goal_reached = bot.wall_follow_goal()
     
@@ -85,4 +47,4 @@ def x():
 
 
 if __name__ == "__main__":
-    x()
+    main()
--- robot.py
+++ robot.py
@@ -1,80 +1,59 @@
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button
-from pybricks.tools import wait
-import math
-
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+import math, time
 
 class Robot:
     def __init__(self):
-        # Hub
         self.brick = EV3Brick()
-<<<<<<< HEAD
         self.length = 0.18
         self.width = 0.122
         self.height = 0.128
         self.wheel_radius = 0.028
 
-=======
 
-        # Geometry (meters)
-        self.wheel_radius = 0.028
-        self.width = 0.111  # distance between wheels
-
-        # Pose in world frame:
-        # x right, y up, theta CCW from +x (radians)
-        self.x = 0.0
-        self.y = 0.0
-        self.theta = 0.0
->>>>>>> efc0cf5e95e54e471201092bd498750626ea97d4
-
-        # Encoder baselines for incremental odometry
         self.prev_left_deg = 0.0
         self.prev_right_deg = 0.0
 
-        # Heading fusion weight (1.0 = gyro only)
         self.GYRO_W = 1.0
 
-<<<<<<< HEAD
 
-        self.start = [0.5 - 0.095, 0 - 0.09, 0]
-        self.goal = [2.5, 2.5, 0]
+        self.start = [0.5 + 0.09, 0 - 0.09, math.radians(90)]
+        self.goal = [2.52, 2.53, 0]
+        self.recent_hitpoint = self.start.copy()
+        self.distance_to_goal = self.distance_between_points(self.start, self.goal)
 
         self.m = ((self.goal[1] - self.start[1]) / (self.goal[0] - self.start[0]))
         self.b = self.goal[1] - self.m * self.goal[0]
 
-=======
-        # Devices
->>>>>>> efc0cf5e95e54e471201092bd498750626ea97d4
         self.left_motor = Motor(Port.B, Direction.CLOCKWISE)
         self.right_motor = Motor(Port.C, Direction.CLOCKWISE)
         self.ultrasonic_sensor = UltrasonicSensor(Port.S1)
         self.touch_sensor_left = TouchSensor(Port.S2)
         self.touch_sensor_right = TouchSensor(Port.S3)
+
         self.gyro = GyroSensor(Port.S4)
-<<<<<<< HEAD
         self.heading_bias_deg = 0.0
-        self.reset_odometry()
-        self.x = self.start[0]
-        self.y = self.start[1]
-        self.theta = self.start[2]
-=======
->>>>>>> efc0cf5e95e54e471201092bd498750626ea97d4
+        self.reset_odometry(self.start[0], self.start[1], self.start[2])
 
-        # Gyro bias so that gyro angle + bias = global heading (deg)
-        self.heading_bias_deg = 0.0
 
-        # Initialize
-        self.reset_odometry(0.0, 0.0, 0.0)
+        # Precompute M-line coefficients for distance calculation
+        x1, y1, _ = self.start
+        x2, y2, _ = self.goal
 
-    # ------------------------------------------------------
-    # Odometry / pose setup
-    # ------------------------------------------------------
+        self.A = y1 - y2
+        self.B = x2 - x1
+        self.C = x1 * y2 - x2 * y1
+
+        self.mline_norm = math.sqrt(self.A**2 + self.B**2)
+
+
     def reset_motor_baselines(self):
         self.prev_left_deg = self.left_motor.angle()
         self.prev_right_deg = self.right_motor.angle()
 
-<<<<<<< HEAD
     def reset_odometry(self, x=0.0, y=0.0, theta=0.0):
         self.x, self.y, self.theta = x, y, theta
         self.left_motor.reset_angle(0)
@@ -106,97 +85,39 @@ class Robot:
                 dth = dth - 2*math.pi
         self.turn(math.degrees(dth), speed)
 
-=======
->>>>>>> efc0cf5e95e54e471201092bd498750626ea97d4
     def initialize_gyroscope(self, reference_theta_rad=0.0):
-        """
-        Align gyro so that its reading corresponds to reference_theta_rad
-        in our global frame (0 = +x, 90° = +y).
-        """
         self.left_motor.stop()
         self.right_motor.stop()
         self.gyro.reset_angle(0)
         wait(1500)
         self.heading_bias_deg = math.degrees(reference_theta_rad)
 
-<<<<<<< HEAD
     def read_gyro_heading_rad(self):
         raw_deg = self.gyro.angle()
         heading_deg = self.heading_bias_deg - raw_deg
         return math.radians(heading_deg)
-=======
-    def reset_odometry(self, x=0.0, y=0.0, theta=0.0):
-        """
-        Reset pose and encoders.
-        theta in radians, CCW from +x.
-        """
-        self.x, self.y, self.theta = x, y, theta
-        self.left_motor.reset_angle(0)
-        self.right_motor.reset_angle(0)
-        self.reset_motor_baselines()
-        self.initialize_gyroscope(reference_theta_rad=theta)
->>>>>>> efc0cf5e95e54e471201092bd498750626ea97d4
 
-    def set_pose_deg(self, x, y, theta_deg):
-        self.reset_odometry(x, y, math.radians(theta_deg))
+    def get_ultrasonic_distance(self):
+        return self.ultrasonic_sensor.distance() / 1000.0
 
-    # ------------------------------------------------------
-    # Basic getters
-    # ------------------------------------------------------
-    def get_x(self):
-        return self.x
+    def get_x(self): return self.x
+    def get_y(self): return self.y
+    def get_theta(self): return self.theta
 
-<<<<<<< HEAD
     def get_pose(self):
         return [self.x, self.y, self.theta]
 
-=======
-    def get_y(self):
-        return self.y
-
-    def get_theta(self):
-        return self.theta  # radians
-
-    def get_theta_deg(self):
-        return math.degrees(self.theta)
-
-    # ------------------------------------------------------
-    # Gyro heading helpers
-    # ------------------------------------------------------
-    def get_heading_deg_from_gyro(self):
-        """
-        Global heading in degrees, from gyro + bias.
-        0 = +x, 90 = +y, CCW positive.
-        """
-        return self.gyro.angle() + self.heading_bias_deg
-
-    def get_heading_rad_from_gyro(self):
-        return math.radians(self.get_heading_deg_from_gyro())
-
-    # Normalize angle to (-180, 180]
-    def _normalize_deg(self, angle):
-        while angle > 180:
-            angle -= 360
-        while angle <= -180:
-            angle += 360
-        return angle
-
-    # ------------------------------------------------------
-    # Odometry core
-    # ------------------------------------------------------
->>>>>>> efc0cf5e95e54e471201092bd498750626ea97d4
     def distance_to_degrees(self, distance):
         return (distance / (2 * math.pi * self.wheel_radius)) * 360.0
 
     def degrees_to_distance(self, degrees):
         return (degrees / 360.0) * (2 * math.pi * self.wheel_radius)
 
-<<<<<<< HEAD
     def creep_forward(self, speed=200):
         self.left_motor.run(speed)
         self.right_motor.run(speed)
 
-    def move_forward(self, distance, speed=200):
+    def move_forward(self, distance, speed=350):
         distance += 0.02
         rotation_degrees = self.distance_to_degrees(distance)
         self.left_motor.run_angle(speed, rotation_degrees, Stop.BRAKE, wait=False)
@@ -204,7 +125,7 @@ class Robot:
 
         self.update_position()
 
-    def move_backward(self, distance, speed=200):
+    def move_backward(self, distance, speed=350):
         self.move_forward(-distance, speed=speed)
 
     
@@ -239,18 +160,6 @@ class Robot:
 
         left_deg  = self.left_motor.angle()
         right_deg = self.right_motor.angle()
-=======
-    def update_odometry(self):
-        """
-        Update (x, y, theta) using encoders for distance and gyro for heading.
-        Call often while moving.
-        """
-        left_deg = self.left_motor.angle()
-        right_deg = self.right_motor.angle()
-
-        dL_deg = left_deg - self.prev_left_deg
-        dR_deg = right_deg - self.prev_right_deg
->>>>>>> efc0cf5e95e54e471201092bd498750626ea97d4
 
         dL = math.radians(left_deg  - self.prev_left_deg) * r
         dR = math.radians(right_deg - self.prev_right_deg) * r
@@ -277,201 +186,28 @@ class Robot:
 
 
 
-    # ------------------------------------------------------
-    # Basic sensing / utility
-    # ------------------------------------------------------
-    def get_ultrasonic_distance(self):
-        return self.ultrasonic_sensor.distance() / 1000.0
+    def show_gyro_angle(self):
+        self.brick.screen.clear()
+        wait(1000)
+
+        while True:
+            angle_deg = math.degrees(self.read_gyro_heading_rad())
+            self.brick.screen.clear()
+            self.brick.screen.print("Angle: " + str(round(angle_deg, 1)))
+            if Button.CENTER in self.brick.buttons.pressed():
+                self.brick.speaker.beep()
+                break
+            wait(100)
+
+    def is_touching_wall(self):
+        return self.touch_sensor_left.pressed() and self.touch_sensor_right.pressed()
+
+    def beep(self):
+        self.brick.speaker.beep()
 
     def wait_for_button_press(self):
         while Button.CENTER not in self.brick.buttons.pressed():
             wait(10)
-
-<<<<<<< HEAD
-    def wall_follow(self, start_pos, return_pos, target_distance=0.15, base_speed=200, kp=375, ki=0, kd=50):
-=======
-    def beep(self):
-        self.brick.speaker.beep()
-
-    # ------------------------------------------------------
-    # Motion: straight line
-    # ------------------------------------------------------
-    def move_forward(self, distance, speed=200):
-        """
-        Move straight by given distance (meters) along current heading.
-        """
-        rot_deg = self.distance_to_degrees(distance)
-
-        self.left_motor.run_angle(speed, rot_deg, Stop.BRAKE, wait=False)
-        self.right_motor.run_angle(speed, rot_deg, Stop.BRAKE, wait=True)
-
-        self.update_odometry()
-
-    def move_backward(self, distance, speed=200):
-        self.move_forward(-distance, speed)
-
-    # ------------------------------------------------------
-    # Heading control: no manual angle math in main
-    # ------------------------------------------------------
-    def turn_to_heading_deg(self, target_deg, speed=120):
-        """
-        Rotate in place until global heading (from gyro) is target_deg.
-        target_deg is in our global frame: 0 = +x, 90 = +y.
-        """
-        target_deg = self._normalize_deg(target_deg)
-
-        while True:
-            current = self._normalize_deg(self.get_heading_deg_from_gyro())
-            error = self._normalize_deg(target_deg - current)
-
-            if abs(error) < 1.0:
-                break
-
-            direction = 1 if error > 0 else -1
-            self.left_motor.run(direction * speed)
-            self.right_motor.run(-direction * speed)
-            wait(10)
-
-        self.left_motor.stop()
-        self.right_motor.stop()
-        wait(100)
-
-        # Sync stored theta + encoder baselines
-        self.theta = self.get_heading_rad_from_gyro()
-        self.reset_motor_baselines()
-
-    def turn(self, angle_deg, speed=120):
-        """
-        Relative turn by angle_deg (deg) using gyro internally.
-        Positive = CCW, Negative = CW.
-        """
-        current = self.get_heading_deg_from_gyro()
-        target = current + angle_deg
-        target = self._normalize_deg(target)
-        self.turn_to_heading_deg(target, speed=speed)
-
-    def turn_towards_goal(self, goal_x, goal_y, speed=120):
-        """
-        Point the robot toward (goal_x, goal_y) in world coordinates.
-        No explicit angle math in main; all internal here.
-        """
-        dx = goal_x - self.x
-        dy = goal_y - self.y
-
-        # atan2 gives desired global heading in our coordinate system
-        desired_theta_rad = math.atan2(dy, dx)
-        desired_deg = math.degrees(desired_theta_rad)
-
-        self.turn_to_heading_deg(desired_deg, speed=speed)
-
-    # ------------------------------------------------------
-    # (Optional) wall_follow – unchanged logic except we
-    # call update_odometry() each loop.
-    # ------------------------------------------------------
-    def wall_follow(self, start_pos, return_pos,
-                    target_distance=0.15, base_speed=200,
-                    kp=375, ki=0, kd=50):
-
-        # Assumes pose already set before calling, ignores start_pos for now.
->>>>>>> efc0cf5e95e54e471201092bd498750626ea97d4
-        self.reset_motor_baselines()
-        last_error = 0.0
-        integral = 0.0
-        I_MAX = 0.2
-<<<<<<< HEAD
-        CORR_CAP = 150.0
-        MIN_SPD, MAX_SPD = 80, 280
-        MIN_TRAVEL_TO_ALLOW_EXIT = 0.40
-=======
-        CORR_CAP = 80.0
-        MIN_SPD, MAX_SPD = 80, 240
-        MIN_TRAVEL_TO_ALLOW_EXIT = 0.35
->>>>>>> efc0cf5e95e54e471201092bd498750626ea97d4
-        traveled_accum = 0.0
-        prev_x, prev_y = self.x, self.y
-
-        def near_return():
-            dx = self.x - return_pos[0]
-            dy = self.y - return_pos[1]
-            return (abs(dx) <= 0.10) and (abs(dy) <= 0.10)
-
-        while True: 
-            distance = self.get_ultrasonic_distance()
-            print(self.x, self.y, self.theta)
-            if self.touch_sensor_left.pressed() or self.touch_sensor_right.pressed():
-                self.move_backward(0.15)
-                self.turn(-90, 100)
-                prev_x, prev_y = self.x, self.y
-                continue
-            if distance <= 0 or distance > 0.6:
-                distance = 0.30
-
-            # Too close to wall: turn away a bit
-            if distance < 0.08:
-                self.turn(-8, 60)
-<<<<<<< HEAD
-                prev_x, prev_y = self.x, self.y
-                continue
-
-=======
-                self.reset_motor_baselines()
-                prev_x, prev_y = self.x, self.y
-                continue
->>>>>>> efc0cf5e95e54e471201092bd498750626ea97d4
-
-            # Bump sensors: back off + turn away
-            if self.touch_sensor_left.pressed() or self.touch_sensor_right.pressed():
-                self.move_backward(0.15)
-                self.turn(-90, 100)
-                self.reset_motor_baselines()
-                prev_x, prev_y = self.x, self.y
-                continue
-
-            # PID control
-            error = target_distance - distance
-            derivative = error - last_error
-            last_error = error
-            integral += error
-            integral = max(-I_MAX, min(I_MAX, integral))
-
-            correction = kp * error + ki * integral + kd * derivative
-            correction = max(-CORR_CAP, min(CORR_CAP, correction))
-
-            left_speed = base_speed + correction
-            right_speed = base_speed - correction
-            left_speed = max(MIN_SPD, min(MAX_SPD, left_speed))
-            right_speed = max(MIN_SPD, min(MAX_SPD, right_speed))
-
-            self.left_motor.run(left_speed)
-            self.right_motor.run(right_speed)
-
-            # Odometry update while moving
-            self.update_odometry()
-
-            dx_step = self.x - prev_x
-            dy_step = self.y - prev_y
-            step = math.sqrt(dx_step * dx_step + dy_step * dy_step)
-            traveled_accum += step
-            prev_x, prev_y = self.x, self.y
-
-            self.brick.screen.clear()
-            dx_ret = abs(self.x - return_pos[0])
-            dy_ret = abs(self.y - return_pos[1])
-            self.brick.screen.print(
-                str(round(dx_ret, 3)) + "\n" +
-                str(round(dy_ret, 3)) + "\n" +
-                str(round(self.theta, 3))
-            )
-
-            if traveled_accum >= MIN_TRAVEL_TO_ALLOW_EXIT and near_return():
-                break
-
-            wait(30)
-
-        self.left_motor.stop()
-        self.right_motor.stop()
-        self.beep()
-<<<<<<< HEAD
 
     def drive_to_point(self, x_tgt, y_tgt, speed=180, stop_tol=0.03, prefer_right=True):
         self.turn_towards_point(x_tgt, y_tgt, prefer_right=prefer_right, speed=100)
@@ -499,13 +235,12 @@ class Robot:
     def near_goal(self):
         dx = self.x - self.goal[0]
         dy = self.y - self.goal[1]
-        return (abs(dx) <= 0.05) and (abs(dy) <= 0.05)
+        return (abs(dx) <= 0.03) and (abs(dy) <= 0.03)
 
     
-    def on_m_line(self):
-        line_y = self.m * self.x + self.b
-        return abs(self.y - line_y) <= 0.10
-
+    def on_m_line(self, tol=0.10):
+        d = abs(self.A * self.x + self.B * self.y + self.C) / self.mline_norm
+        return d <= tol
 
 
     def wall_follow_goal(self, target_distance=0.15, base_speed=200, kp=375, ki=0, kd=50):
@@ -524,13 +259,13 @@ class Robot:
            # print(self.x, self.y, self.theta)
             if self.touch_sensor_left.pressed() or self.touch_sensor_right.pressed():
                 self.move_backward(0.15)
-                self.turn(-90, 100)
+                self.turn(-90, 200)
                 prev_x, prev_y = self.x, self.y
                 continue
             if distance <= 0 or distance > 0.6:
                 distance = 0.30
             if distance < 0.08:
-                self.turn(-8, 60)
+                self.turn(-15, 150)
                 prev_x, prev_y = self.x, self.y
                 continue
 
@@ -573,5 +308,9 @@ class Robot:
         self.left_motor.stop()
         self.right_motor.stop()
         self.beep()
-=======
->>>>>>> efc0cf5e95e54e471201092bd498750626ea97d4
+
+
+    def distance_between_points(self, p1, p2):
+        x1, y1, _ = p1
+        x2, y2, _ = p2
+        return math.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2)
